ef799222198b7630d07cae2426ad2d3065f6306b
Gurunarendra46 <86468947+Gurunarendra46@users.noreply.github.com>
2021-07-23 16:51:05 +0530

    Add files via upload

---
+++ Authentication/main.js
@@ -0,0 +1,94 @@
+// Create a registration system
+var objPeople = [
+	{
+		username: 'guru',
+		password: '8333847791'
+	},
+	{
+		username: 'jay',
+		password: '87654321'
+	},
+	{
+		username: 'tcs',
+		password: 'tcs12345'
+	}
+]
+
+
+function login() {
+    //var objPeople = new Array();
+    //objPeople = localStorage.getItem("all_users");
+    //var data2 = JSON.parse(objPeople);
+    //console.log(data2)
+    //console.log(objPeople)
+    //objPeople = localStorage.getItem('all_users',JSON.stringify(objPeople));
+    //console.log('retrieveddata', JSON.parse(retrievedata));
+    //objPeople=(localStorage.getItem("all_users"));
+    //objPeople.push({username: username, password: password});
+    //localStorage.setItem('username',JSON.stringify(objPeople));
+    //localStorage.setItem('password',JSON.stringify(objPeople));
+
+    //var getJson = localStorage.getItem('all_users')
+    //arrayName = JSON.parse(getJson)
+
+   // var pull=JSON.parse(localStorage.getItem('all_users'));
+
+	var username = document.getElementById('username').value
+
+	var password = document.getElementById('password').value
+
+
+	for(var i = 0; i < objPeople.length; i++) {
+
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			console.log(username + ' ,you are now logged ')
+			location.href = 'https://cloudtechin.blogspot.com/';
+			break
+		} else {
+			console.log('incorrect username or password')
+			alert('Invalid username or Password')
+			location.href = 'pasword.html';
+			break
+		}
+	}
+}
+
+
+function registerUser() {
+
+    var registerfullname = document.getElementById('newfullname').value
+    var registerEmail = document.getElementById('newEmail').value
+	var registerUsername = document.getElementById('newUsername').value
+	var registerPassword = document.getElementById('newPassword').value
+	//sessionStorage.setItem(“currentloggedin”,registerUsername);
+	//sessionStorage.setItem(“currentloggedin”,registerPassword);
+	var newUser = {
+		username: registerUsername,
+		password: registerPassword
+	}
+	for(var i = 0; i < objPeople.length; i++) {
+		if(registerUsername == objPeople[i].username) {
+
+			alert('That username is already in exist, please sign in with ur details')
+
+			break
+
+		} else if (registerPassword.length < 8) {
+
+			alert('That is very short password, include > 8 characters')
+
+			break
+		}
+		else{
+		alert('successfully registered')
+		location.href = 'pasword.html';
+
+		break
+		}
+	}
+
+	objPeople.push(newUser)
+	localStorage.setItem('all_users',JSON.stringify(objPeople));
+
+	console.log(objPeople)
+}
